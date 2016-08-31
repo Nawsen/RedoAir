620165e2da994166832d26067ba6dee7cf822678
Wannes Van Dorpe <wannes.vandorpe@realdolmen.com>
2016-08-31 11:49:01 +0200

    changed pom to include backend in frontends + added dummy data for airports

---
+++ Backend/src/main/resources/Airports.sql
@@ -0,0 +1,10 @@
+INSERT INTO airport (code,city,country,region,name) VALUES ('BRU-001','Brussel','Belgium','Europa','Brussels Airport');
+INSERT INTO airport (code,city,country,region,name) VALUES ('OST-002','Oostende','Belgium','Europa','Ostend vlieghaven');
+INSERT INTO airport (code,city,country,region,name) VALUES ('FLA-542','Atlanta','United States','America',' Hartsfield–Jackson Atlanta International Airport');
+INSERT INTO airport (code,city,country,region,name) VALUES ('BCI-584','Beijing','China','Azia','Beijing Capital International Airport');
+INSERT INTO airport (code,city,country,region,name) VALUES ('dub-071','Dubai','United Arab Emirates','Africa','Dubai International Airport');
+INSERT INTO airport (code,city,country,region,name) VALUES ('TOK-458','Tokyo','Japan','Azia','Tokyo Haneda Airport');
+INSERT INTO airport (code,city,country,region,name) VALUES ('LAS-658','California','United States','America','Los Angeles International Airport');
+INSERT INTO airport (code,city,country,region,name) VALUES ('PCG-875','Île-de-France','France','Europa','Paris-Charles de Gaulle Airport');
+INSERT INTO airport (code,city,country,region,name) VALUES ('DME-722','Istanbul','Turkey','Africa','Istanbul Atatürk Airport');
+INSERT INTO airport (code,city,country,region,name) VALUES ('SCA-555','Changi','Singapore','Azie','Singapore Changi Airport');

f442882570c95da30764d9ac9fb4b97cf346908f
Mathias Bulté <bulte.mathias@gmail.com>
2016-08-31 14:12:36 +0200

    Added webapp stuff

--- Frontend-Public/src/main/webapp/gulpfile.js
+++ Frontend-Public/src/main/webapp/gulpfile.js
@@ -1,16 +1,39 @@
-'use strict';
+const gulp = require('gulp');
+const HubRegistry = require('gulp-hub');
+const browserSync = require('browser-sync');
 
-var gulp = require('gulp');
+const conf = require('./conf/gulp.conf');
 
-gulp.paths = {
-  src: 'src',
-  dist: 'dist',
-  tmp: '.tmp',
-  e2e: 'e2e'
-};
+// Load some files into the registry
+const hub = new HubRegistry([conf.path.tasks('*.js')]);
 
-require('require-dir')('./gulp');
+// Tell gulp to use the tasks just loaded
+gulp.registry(hub);
 
-gulp.task('default', ['clean'], function () {
-    gulp.start('build');
-});
+gulp.task('inject', gulp.series(gulp.parallel('styles', 'scripts'), 'inject'));
+gulp.task('build', gulp.series('partials', gulp.parallel('inject', 'other'), 'build'));
+gulp.task('test', gulp.series('scripts', 'karma:single-run'));
+gulp.task('test:auto', gulp.series('watch', 'karma:auto-run'));
+gulp.task('serve', gulp.series('inject', 'watch', 'browsersync'));
+gulp.task('serve:dist', gulp.series('default', 'browsersync:dist'));
+gulp.task('default', gulp.series('clean', 'build'));
+gulp.task('watch', watch);
+
+function reloadBrowserSync(cb) {
+  browserSync.reload();
+  cb();
+}
+
+function watch(done) {
+  gulp.watch([
+    conf.path.src('index.html'),
+    'bower.json'
+  ], gulp.parallel('inject'));
+
+  gulp.watch(conf.path.src('app/**/*.html'), reloadBrowserSync);
+  gulp.watch([
+    conf.path.src('**/*.css')
+  ], gulp.series('styles'));
+  gulp.watch(conf.path.src('**/*.js'), gulp.series('inject'));
+  done();
+}

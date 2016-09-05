af793f761b7f264fc57ad0a9bd39d8d764cbe3b8
Wannes Van Dorpe <wannes.vandorpe@realdolmen.com>
2016-09-05 13:52:12 +0200

    db import applicationsettings

---
+++ Backend/src/main/resources/ApplicationSettings.sql
@@ -0,0 +1,3 @@
+INSERT INTO `realdolmen`.`application_settings` (id,setting_name,valid_for,setting_value) VALUES (0,'JWT_SECRET',null,'o^qsdifhzOER%PPPPJIROGJMFD%F*F¨PP¨R340°RSJOF°45QZËRTZ86FE6FZEQF34YG4D64EY890986046556SRF486SD6F7Z/9EARZEZFSD3Y6TZE0°OQSDPKCVJHBNKX?WL.M/QPAZO0QSKLDJIOERUOZPEieçrojsd785yhàçueiozd89frt4y57894tfgr46y68y7e4s6d8rf4tioe!è§t(''!"çàé)-à''(ç§!rieokdflmse$^z"''-)ze$y');
+INSERT INTO `realdolmen`.`application_settings` (id,setting_name,valid_for,setting_value) VALUES (1,'JWT_HOST',null,'http://localhost:8080');
+INSERT INTO `realdolmen`.`application_settings` (id,setting_name,valid_for,setting_value) VALUES (2,'JWT_VALID_FOR_SECONDS',null,'60');

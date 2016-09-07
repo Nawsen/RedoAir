b9674f9322a7e1438a3821b154cab7db0b325fba
Wannes Van Dorpe <wannes.vandorpe@realdolmen.com>
2016-09-07 13:25:13 +0200

    fixed auth

--- Backend/src/main/java/com/realdolmen/filters/AuthFilter.java
+++ Backend/src/main/java/com/realdolmen/filters/AuthFilter.java
@@ -1,10 +1,10 @@
 package com.realdolmen.filters;
 
 
-import com.auth0.jwt.JWTVerifier;
-import com.auth0.jwt.JWTVerifyException;
 import com.realdolmen.qualifiers.Auth;
 import com.realdolmen.repository.ApplicationSettingsRepository;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 
 import javax.inject.Inject;
 import javax.ws.rs.WebApplicationException;
@@ -12,7 +12,6 @@ import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
-import java.util.Map;
 
 /**
  * Created by WVDAZ49 on 2/09/2016.
@@ -26,13 +25,21 @@ public class AuthFilter implements ContainerRequestFilter {
 
     @Override
     public void filter(ContainerRequestContext requestContext) {
-        final String secret = applicationSettingsRepository.findValue("JWT_SECRET");
+        final String KEY = applicationSettingsRepository.findValue("JWT_SECRET");
+
         try {
-            final JWTVerifier verifier = new JWTVerifier(secret);
-            final Map<String,Object> claims = verifier.verify(requestContext.getHeaderString("Authorization").split(" ")[1]);
-            requestContext.getHeaders().add("email", claims.get("email").toString());
-        } catch (JWTVerifyException e) {
-            throw new WebApplicationException(Response.status(Response.Status.UNAUTHORIZED).build());
+            if (requestContext.getHeaderString("Authorization") != null) {
+                //we need to split off the non usefull things from the header
+                //in this case we need to chew off "Bearer "
+                String code = requestContext.getHeaderString("Authorization").split(" ")[1];
+                Claims claim = Jwts.parser().setSigningKey(KEY).parseClaimsJws(code).getBody();
+                //after we check if the claim is correct lets set the email into the header
+                //with this we can identify the user
+                requestContext.getHeaders().add("email", claim.getId());
+            } else {
+                throw new WebApplicationException(Response.status(Response.Status.UNAUTHORIZED).build());
+            }
+            //requestContext.getHeaders().add("email", claims.get("email").toString());
         } catch (Exception e) {
             throw new WebApplicationException(Response.status(Response.Status.UNAUTHORIZED).build());
         }

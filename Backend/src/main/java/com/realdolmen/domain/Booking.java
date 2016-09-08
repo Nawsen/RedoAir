4c448fa9574e4d2ba72b2f4131e3e5371765b46d
Wannes Van Dorpe <wannes.vandorpe@realdolmen.com>
2016-09-08 09:15:33 +0200

    fixed hibernate JPA bork

--- Backend/src/main/java/com/realdolmen/domain/Booking.java
+++ Backend/src/main/java/com/realdolmen/domain/Booking.java
@@ -11,7 +11,7 @@ public class Booking {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @OneToMany
+    @OneToMany(fetch = FetchType.EAGER)
     @ElementCollection
     private List<Ticket> tickets;
     private Boolean paid;
